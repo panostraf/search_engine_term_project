0b94b854dd14b8578ed6b74dde48dc572d2c8e66
panostraf <p.trafalis@gmail.com>
2021-10-23 13:16:25 +0300

    spider update to follow links

--- techradar/techradar/spiders/reviews.py
+++ techradar/techradar/spiders/reviews.py
@@ -3,49 +3,77 @@ from ..items import TechradarItem
 import time
 import random
 import re
+from bs4 import BeautifulSoup
 
 class ReviewsSpider(scrapy.Spider):
     
 
     name = 'reviews'
-    # allowed_domains = ['https://www.techradar.com/']
     start_urls = [f'https://www.techradar.com/reviews/archive/2020/1/']
     month = 2
-    # year = 2020
-    # def __init__(self):
-    #     self.year = 2020
-    #     self.month = 1
     
 
     def parse(self, response, **kwargs):
+        all_links = []
         items = TechradarItem()
-        scrapped_links = response.xpath('//div/a/@href').extract()
-        self.month += 1
+        # xpath to extract the links from each page
+        scrapped_links = response.xpath('//div/a/@href').getall()
+        scrapped_links2 = response.xpath('//li/a/@href').getall()
+        all_links.extend(scrapped_links)
+        all_links.extend(scrapped_links2)
         print(response)
         
-        
-        ReviewsSpider.month += 1
+        # self.month += 1
+        ReviewsSpider.month += 1 # increment month for link pagination
         next_page = f'https://www.techradar.com/reviews/archive/2020/{str(ReviewsSpider.month)}/'
-        # print(next_page)
-        for link in scrapped_links:
+        for link in all_links:
+            print(link)
+        for link in all_links:
+            
+            
             try:
-                if len(re.findall(r"https://www.techradar.com/(review|news)/.*", link)) > 0:
+                # Get only the links from news or reviews using regex
+                if len(re.findall(r"https://www.techradar.com/(reviews|news)/.*", link)) > 0:
                     
-                    # if link not in items['links']:
+                    
+                    # Here we can add a function to visit the link and extract it's text
+                    time.sleep(random.randint(1,4))
+                    yield scrapy.Request(link, callback = self.parse_link_contents)
+                    
                     items['links'] = link
                     print(link)
                     yield items
 
             except IndexError:
-                print('ERROR---------------------')
                 # response.follow(next_page, callback=self.parse)
                 pass
 
-        
+        # Sleep before the next request
         time.sleep(random.randint(1,4))
-        if (ReviewsSpider.month) < 12:
+        if (ReviewsSpider.month) < 13:
+            # Callback on it's self
             yield response.follow(next_page, callback=self.parse)
 
+    
+    def parse_link_contents(self,response,**kwargs):
+        content = response.text
+        soup = BeautifulSoup(content, 'html.parser')
+        content = soup.prettify()
+        link_name = response.url.split('/')[-1]
+        
+        print(link_name)
+        
+        with open(f'downloads/{link_name}.html','w') as f:
+            f.write(content)
+        f.close()
+
+
+
+
+    # name = 'reviews'
+    # start_urls = [f'https://www.techradar.com/reviews/archive/2020/1/']
+    # month = 2
+    
 
 # Proxy rotation
 # Follow links
