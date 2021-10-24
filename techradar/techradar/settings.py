dea9c9c86e69a59d9a8c87fcf8175ee564f6b109
panostraf <p.trafalis@gmail.com>
2021-10-24 21:23:18 +0300

    bug on deploy

--- techradar/techradar/settings.py
+++ techradar/techradar/settings.py
@@ -49,7 +49,8 @@ ROBOTSTXT_OBEY = False
 #    'techradar.middlewares.TechradarSpiderMiddleware': 543,
 #}
 # save_paths = "/Users/panos/Documents/term_project/techradar/techradar/spiders/download_documents/articles_2020.xml"
-save_paths = "./techradar/techradar/spiders/download_documents/articles_2020.xml"
+root_path = "/techradar/techradar/spiders/download_documents/articles_2020.xml"
+save_paths = str(os.path.dirname(os.path.abspath("app.py"))) + root_path
 
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
